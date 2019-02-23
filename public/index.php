5f547d68252943e8caf47fea8c046566154a61e0
Ali Safar AlAmari <asalamri@tahakom.com>
2019-02-23 15:28:19 +0300

    fix

--- public/index.php
+++ public/index.php
@@ -23,9 +23,10 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-control-Allow-credentials: true');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization');
+
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
