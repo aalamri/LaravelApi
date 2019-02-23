67b3614518a8f8942b9c79ac839584d6ff054fc7
Ali Safar AlAmari <asalamri@tahakom.com>
2019-02-23 12:40:28 +0300

    update index

--- public/index.php
+++ public/index.php
@@ -23,9 +23,9 @@ define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers 
+//header('Access-Control-Allow-Origin: *');
+//header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
